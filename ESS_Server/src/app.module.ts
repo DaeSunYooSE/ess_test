c451a7cf25bbe76e5fd6093287d3c4edeb710073
YooDaeSunSTND <daesun.yoo@stndenergy.com>
2022-03-28 06:29:55 +0000

    Add redux-toolkit

--- ESS_Server/src/app.module.ts
+++ ESS_Server/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [MongooseModule.forRoot("mongodb://172.17.0.4:27017/nest"),
     ServeStaticModule.forRoot({
     rootPath: join(__dirname,"..","client")
-    })
+    }),
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- ESS_Server/src/main.ts
+++ ESS_Server/src/main.ts
@@ -6,6 +6,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, "..", "client"))
-  await app.listen(3000);
+  await app.listen(3101);
 }
 bootstrap();
