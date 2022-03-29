a5dca2e486b5460eb0d9c6aa6d6dd5f6da226cf1
YooDaeSunSTND <daesun.yoo@stndenergy.com>
2022-03-29 04:22:36 +0000

    fix folder-name : ESS_Client -> client, ESS_Server -> server

--- ESS_Client/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  exportPathMap: () => ({ // Next Export 에서 사용하는 값
-    "/": { page: "/" },
-  })
-}
-
-export default nextConfig
--- ESS_Server/src/auth/jwt/constants.ts
+++
@@ -1,4 +0,0 @@
-export const jwtConstants = {
-    secret: 'secretKey',
-    expireIn : '60m'
-}
