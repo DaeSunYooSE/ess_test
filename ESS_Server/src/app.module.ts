822a7fa974150b4fc5e50938a9169c56e5db52b9
YooDaeSunSTND <daesun.yoo@stndenergy.com>
2022-03-29 02:10:07 +0000

    In server, Add passport&jwt. And create user, login, get profile for example

--- ESS_Server/src/app.module.ts
+++ ESS_Server/src/app.module.ts
@@ -5,15 +5,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { UserModule } from './user/user.module';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [MongooseModule.forRoot("mongodb://172.17.0.4:27017/nest"),
     ServeStaticModule.forRoot({
     rootPath: join(__dirname,"..","client")
     }),
-    UserModule
+    UserModule,
+    AuthModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
--- ESS_Server/src/user/user.controller.ts
+++ ESS_Server/src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { registerUser } from 'src/type/user.type';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(
+        private userService: UserService
+    ) { }
+
+    @Post()
+    async create(@Body() user:registerUser) {
+        return this.userService.create(user)
+    }
+
+}
